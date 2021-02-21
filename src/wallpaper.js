6ede93a148de0a8892328013b3e3e4fccc344681
SJ <smart5597@naver.com>
2021-02-21 17:38:19 +0900

    Implemented showing wallpaper

---
+++ src/wallpaper.js
@@ -0,0 +1,27 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 3;
+
+function handleImageLoad(){
+    console.log("finished loading");
+}
+
+function showImage(imgNum){
+    const image = new Image();
+    image.src = `/src/images/${imgNum + 1}.webp`;
+    image.classList.add("bgImage");
+    body.appendChild(image);
+    image.addEventListener("loadend", handleImageLoad);
+}
+
+function genRandom(){
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init() {
+    const randomNumber = genRandom();
+    showImage(randomNumber);
+}
+
+init();
